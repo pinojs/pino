08b9f7626f67e48ae35648ab4c0063234a8c56c0
Matteo Collina <hello@matteocollina.com>
2016-06-14 18:19:17 +0100

    test pretty({ timeTransOnly: true })

--- test/pretty.test.js
+++ test/pretty.test.js
@@ -6,7 +6,7 @@ var os = require('os')
 var split = require('split2')
 var hostname = os.hostname()
 
-test('pino transform', function (t) {
+test('pino transform prettifies', function (t) {
   t.plan(4)
   var pretty = pino.pretty()
   pretty.pipe(split(function (line) {
@@ -14,6 +14,19 @@ test('pino transform', function (t) {
     t.ok(line.match(/.*INFO.*/), 'includes level')
     t.ok(line.indexOf('' + process.pid) > 0, 'includes pid')
     t.ok(line.indexOf('' + hostname) > 0, 'includes hostname')
+    return line
+  }))
+  var instance = pino(pretty)
+
+  instance.info('hello world')
+})
+
+test('pino transform can just parse the dates', function (t) {
+  t.plan(1)
+  var pretty = pino.pretty({ timeTransOnly: true })
+  pretty.pipe(split(function (line) {
+    var obj = JSON.parse(line)
+    t.ok(typeof obj.time === 'string', 'time is a string')
     return line
   }))
   var instance = pino(pretty)
