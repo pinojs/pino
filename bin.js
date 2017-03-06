913abdfc9364d98dfe5bcfe8eb05c4f393b8ab38
Joey van Dijk <mail@sayyup.nl>
2017-03-06 21:53:33 +0100

    remove check as requested by davidmarkclements

--- bin.js
+++ bin.js
@@ -5,25 +5,3 @@
 var pretty = require('./pretty')
 
 module.exports = pretty
-
-if (require.main === module) {
-  if (arg('-h') || arg('--help')) {
-    usage().pipe(process.stdout)
-  } else if (arg('-v') || arg('--version')) {
-    console.log(require('./package.json').version)
-  } else {
-    process.stdin.pipe(pretty({
-      timeTransOnly: arg('-t'),
-      levelFirst: arg('-l')
-    })).pipe(process.stdout)
-  }
-}
-
-function usage () {
-  return require('fs')
-      .createReadStream(require('path').join(__dirname, 'usage.txt'))
-}
-
-function arg (s) {
-  return !!~process.argv.indexOf(s)
-}
