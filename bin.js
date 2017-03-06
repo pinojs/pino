172a1450efc4337f69b1c58f145fcb03f686846a
Joey van Dijk <mail@sayyup.nl>
2017-03-06 22:07:00 +0100

    fixed correct remove of require-check

--- bin.js
+++ bin.js
@@ -5,3 +5,23 @@
 var pretty = require('./pretty')
 
 module.exports = pretty
+
+if (arg('-h') || arg('--help')) {
+  usage().pipe(process.stdout)
+} else if (arg('-v') || arg('--version')) {
+  console.log(require('./package.json').version)
+} else {
+  process.stdin.pipe(pretty({
+    timeTransOnly: arg('-t'),
+    levelFirst: arg('-l')
+  })).pipe(process.stdout)
+}
+
+function usage () {
+  return require('fs')
+      .createReadStream(require('path').join(__dirname, 'usage.txt'))
+}
+
+function arg (s) {
+  return !!~process.argv.indexOf(s)
+}
