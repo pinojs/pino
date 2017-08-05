739cb9921274bae2b5b56511b937dfefaa5f3fc4
victor0801x <victor0801x@gmail.com>
2017-08-05 15:49:49 +0800

    change pretty default output localTime
    change pretty.formatTime parse to localTime
    (original is convert date.toISOString())

--- pretty.js
+++ pretty.js
@@ -23,6 +23,37 @@ var standardKeys = [
   'v'
 ]
 
+var localTime = {
+  dateTime: 'YYYY-MM-DD hh:mm:ss.SSS', // '2017-08-05 12:30:45.789'
+  dateTimeTZ: 'YYYY-MM-DDThh:mm:ss.SSSTZ', // '2017-08-05 12:30:45.789 +08:00'
+  time: 'hh:mm:ss',
+  milliTime: 'hh:mm:ss.SSS'
+}
+
+// cache, compute once for timezone-offset
+var tz = _localTimeOffset()
+
+function _localTimeOffset (aMinTimeoffset) {
+  // +/- minute timeoffset
+  var tz = aMinTimeoffset || new Date().getTimezoneOffset()
+  var tmp = Math.abs(tz)
+  // es2017
+  // var offset = String(Math.floor(tmp / 60)).padStart(2, '0') + ':' + String(tmp % 60).padStart(2, '0')
+  var offset = _lpadzero(String(Math.floor(tmp / 60)), 2) + ':' + _lpadzero(String(tmp % 60), 2)
+  return tz > 0 ? '-' + offset : '+' + offset
+}
+
+function _lpadzero (aTarget, aLength, aPadChar) {
+  var char = aPadChar || '0'
+  var targetStr = aTarget.toString()
+  var times = aLength - targetStr.length
+  var padding = ''
+  while ((times--) > 0) {
+    padding += char
+  }
+  return padding + targetStr
+}
+
 function withSpaces (value) {
   var lines = value.split('\n')
   for (var i = 1; i < lines.length; i++) {
@@ -130,13 +161,43 @@ function pretty (opts) {
     return new Date(time).toISOString()
   }
 
+  // TODO:
+  // 1. need validate custom format string
+  // 2. more format string support
+  function asLocalDate (aTime, aFormat) {
+    var time = aTime
+    var format = aFormat
+    if (!format) {
+      format = localTime.dateTimeTZ
+    }
+    var date = new Date(time)
+    var year = date.getFullYear()
+    var month = _lpadzero(date.getMonth() + 1, 2)
+    var day = _lpadzero(date.getDate(), 2)
+    var hour = _lpadzero(date.getHours(), 2)
+    var minute = _lpadzero(date.getMinutes(), 2)
+    var second = _lpadzero(date.getSeconds(), 2)
+    var milli = _lpadzero(date.getMilliseconds(), 3)
+
+    var _format = format
+      .replace(/YYYY/g, year)
+      .replace(/MM/g, month)
+      .replace(/DD/g, day)
+      .replace(/hh/g, hour)
+      .replace(/mm/g, minute)
+      .replace(/ss/g, second)
+      .replace(/SSS/g, milli)
+      .replace(/TZ/g, tz || _localTimeOffset())
+    return _format
+  }
+
   function formatTime (value, after) {
     after = after || ''
     try {
       if (!value || !value.time) {
         return ''
       } else {
-        return '[' + asISODate(value.time) + ']' + after
+        return '[' + asLocalDate(value.time) + ']' + after
       }
     } catch (_) {
       return ''
@@ -153,3 +214,4 @@ function pretty (opts) {
 }
 
 module.exports = pretty
+
