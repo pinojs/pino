001736b9b9d442408083fee56ff17589c9a066f0
Tobias Nießen <tniessen@tnie.de>
2017-11-02 17:25:16 +0100

    Update unit test

--- test/crlf.test.js
+++ test/crlf.test.js
@@ -2,41 +2,56 @@
 
 var test = require('tap').test
 var pino = require('../')
-var Writable = require('stream').Writable
+var pretty = require('../pretty')
+var writer = require('flush-write-stream')
 
-class LogWriteStream extends Writable {
-  constructor () {
-    super()
-    this.data = ''
-  }
-
-  write (data) {
-    this.data += data
-  }
-
-  toString () {
-    return this.data
-  }
+function capture () {
+  var ws = writer(function (chunk, enc, cb) {
+    this.data += chunk.toString()
+    cb()
+  })
+  ws.data = ''
+  return ws
 }
 
 test('pino uses LF by default', function (t) {
   t.plan(1)
-  var stream = new LogWriteStream()
+  var stream = capture()
   var logger = pino(stream)
   logger.info('foo')
   logger.error('bar')
-  t.ok(/foo[^\r\n]+\n[^\r\n]+bar[^\r\n]+\n/.test(stream.toString()));
+  t.ok(/foo[^\r\n]+\n[^\r\n]+bar[^\r\n]+\n/.test(stream.data))
   t.end()
 })
 
 test('pino can log CRLF', function (t) {
   t.plan(1)
-  var stream = new LogWriteStream()
+  var stream = capture()
   var logger = pino({
     crlf: true
   }, stream)
   logger.info('foo')
   logger.error('bar')
-  t.ok(/foo[^\n]+\r\n[^\n]+bar[^\n]+\r\n/.test(stream.toString()));
+  t.ok(/foo[^\n]+\r\n[^\n]+bar[^\n]+\r\n/.test(stream.data))
+  t.end()
+})
+
+test('pretty can log CRLF', function (t) {
+  t.plan(1)
+  var prettier = pretty({
+    crlf: true,
+    formatter: function (data) {
+      return data.msg
+    }
+  })
+
+  var stream = capture()
+  prettier.pipe(stream)
+
+  var logger = pino(prettier)
+  logger.info('foo')
+  logger.info('bar')
+
+  t.is(stream.data, 'foo\r\nbar\r\n')
   t.end()
 })
