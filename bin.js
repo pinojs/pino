f1ae8b6993057ca52e9c0f92ba9d2a547cacb156
James Sumners <james.sumners@gmail.com>
2018-04-05 13:37:22 -0400

    Add URL to deprecation notice

--- bin.js
+++ bin.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
-console.error('`pino` cli is deprecated. Use `pino-pretty` cli instead.')
+console.error(
+  '`pino` cli is deprecated. Use `pino-pretty` cli instead.\n' +
+  '\nSee: https://github.com/pinojs/pino-pretty'
+)
 process.exit(1)
