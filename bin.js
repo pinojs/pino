71e86d12362c68db6f5608c69e58a052bc898a9e
James Sumners <james.sumners@gmail.com>
2018-04-06 14:30:30 -0400

    deprecated => removed

--- bin.js
+++ bin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 console.error(
-  '`pino` cli is deprecated. Use `pino-pretty` cli instead.\n' +
+  '`pino` cli has been removed. Use `pino-pretty` cli instead.\n' +
   '\nSee: https://github.com/pinojs/pino-pretty'
 )
 process.exit(1)
