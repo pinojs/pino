1ef59f2a779638672de190461d9b1911a7f1c61d
davidmarkclements <huperekchuno@googlemail.com>
2018-06-16 01:37:33 +0200

    tests: remove use of timers in events tests

--- test/events.test.js
+++ test/events.test.js
@@ -4,97 +4,61 @@ const { test } = require('tap')
 const { join } = require('path')
 const { fork, spawn } = require('child_process')
 const { once } = require('./helper')
-const writer = require('flush-write-stream')
 
 const fixtures = join(__dirname, 'fixtures', 'events')
 
 test('no event loop logs successfully', async ({is}) => {
-  var output = ''
   const child = fork(join(fixtures, 'no-event-loop.js'), {silent: true})
-  child.stdout.pipe(writer((s, enc, cb) => {
-    output += s
-    cb()
-  }))
+  const output = await once(child.stdout, 'data')
   await once(child, 'close')
-  is(/"msg":"h"/.test(output), true)
+  is(/"msg":"h"/.test(await output), true)
 })
 
 test('terminates when uncaughtException is fired with onTerminate registered', async ({is}) => {
-  var output = ''
-  var errorOutput = ''
   const child = spawn(process.argv[0], [join(fixtures, 'uncaught-exception.js')], {silent: true})
-
-  child.stdout.pipe(writer((s, enc, cb) => {
-    output += s
-    cb()
-  }))
-
-  child.stderr.pipe(writer((s, enc, cb) => {
-    errorOutput += s
-    cb()
-  }))
+  const output = once(child.stdout, 'data')
+  const output2 = output.then(() => once(child.stdout, 'data'))
+  const errorOutput = once(child.stderr, 'data')
   await once(child, 'close')
-  is(/"msg":"h"/.test(output), true)
-  is(/terminated/g.test(output), true)
-  is(/this is not caught/g.test(errorOutput), true)
+  is(/"msg":"h"/.test(await output), true)
+  is(/terminated/g.test(await output2), true)
+  is(/this is not caught/g.test(await errorOutput), true)
 })
 
 test('terminates when uncaughtException is fired without onTerminate registered', async ({is}) => {
-  var output = ''
   const child = spawn(process.argv[0], [join(fixtures, 'uncaught-exception-no-terminate.js')], {silent: true})
-
-  child.stdout.pipe(writer((s, enc, cb) => {
-    output += s
-    cb()
-  }))
+  const output = once(child.stdout, 'data')
   const code = await once(child, 'exit')
   is(code, 1)
-  is(/"msg":"h"/.test(output), true)
+  is(/"msg":"h"/.test(await output), true)
 })
 
 test('terminates on SIGHUP when no other handlers registered', async ({is}) => {
-  var output = ''
   const child = spawn(process.argv[0], [join(fixtures, 'sighup-no-handler.js')], {silent: true})
-
-  child.stdout.pipe(writer(function (s, enc, cb) {
-    output += s
-    cb()
-  }))
-
-  child.stderr.pipe(process.stdout)
-  setTimeout(() => child.kill('SIGHUP'), 2000)
+  await once(child.stdout, 'data')
+  const output = once(child.stdout, 'data')
+  child.kill('SIGHUP')
   const code = await once(child, 'exit')
   is(code, 0)
-  is(/"msg":"h"/.test(output), true)
+  is(/"msg":"h"/.test(await output), true)
 })
 
 test('lets app terminate when SIGHUP received with multiple handlers', async ({is}) => {
-  var output = ''
   const child = spawn(process.argv[0], [join(fixtures, 'sighup-with-handler.js')], {silent: true})
-
-  child.stdout.pipe(writer(function (s, enc, cb) {
-    output += s
-    cb()
-  }))
-  setTimeout(() => child.kill('SIGHUP'), 2000)
+  await once(child.stdout, 'data')
+  const output = once(child.stdout, 'data')
+  const output2 = output.then(() => once(child.stdout, 'data'))
+  child.kill('SIGHUP')
   const code = await once(child, 'exit')
   is(code, 0)
-  is(/"msg":"h"/.test(output), true)
-  is(/app sighup/.test(output), true)
+  is(/"msg":"h"/.test(await output + ''), true)
+  is(/app sighup/.test(await output2 + ''), true)
 })
 
 test('destination', async ({is}) => {
-  var output = ''
   const child = spawn(process.argv[0], [join(fixtures, 'destination.js')], {silent: true})
-
-  child.stdout.pipe(writer(function (s, enc, cb) {
-    output += s
-    cb()
-  }))
-
-  child.stderr.pipe(process.stdout)
-
+  const output = once(child.stdout, 'data')
   const code = await once(child, 'exit')
   is(code, 0)
-  is(/"msg":"h"/.test(output), true)
+  is(/"msg":"h"/.test(await output), true)
 })
