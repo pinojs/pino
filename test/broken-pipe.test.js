5dc440bd4fc734e41353c92f45eb70d82bf78df8
Matteo Collina <hello@matteocollina.com>
2018-11-06 00:46:32 +0000

    100% code coverage

--- test/broken-pipe.test.js
+++ test/broken-pipe.test.js
@@ -4,6 +4,7 @@ const t = require('tap')
 const { join } = require('path')
 const { fork } = require('child_process')
 const { once } = require('./helper')
+const pino = require('..')
 
 function test (file) {
   file = join('fixtures', 'broken-pipe', file)
@@ -23,3 +24,19 @@ t.jobs = 42
 test('basic.js')
 test('destination.js')
 test('extreme.js')
+
+t.test('let error pass through', ({ is, plan }) => {
+  plan(3)
+  const stream = pino.destination()
+
+  // side effect of the pino constructor is that it will set an
+  // event handler for error
+  pino(stream)
+
+  process.nextTick(() => stream.emit('error', new Error('kaboom')))
+  process.nextTick(() => stream.emit('error', new Error('kaboom')))
+
+  stream.on('error', (err) => {
+    is(err.message, 'kaboom')
+  })
+})
