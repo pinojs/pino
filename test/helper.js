56873a27870264a0a3f900d4276dae0f03c1444d
Jimmie Hansson <jimmiehansson1985@gmail.com>
2018-12-06 11:14:01 +0100

    PR-561 Add inline documentation to clarify the usage and purpose of the helper

--- test/helper.js
+++ test/helper.js
@@ -7,6 +7,28 @@ const pid = process.pid
 const hostname = os.hostname()
 const v = 1
 
+/**
+ * @function expected
+ * @access public
+ *
+ * @description
+ * DOING: Should return the exposed members of the proxy as invocables.
+ * This function is intended to evaluate the expression of formats used
+ * in string literals against matching types for assertions.
+ *
+ * @example
+ *
+ * @See output$
+ * @inner
+ * // return expected('123 abc').output({ has: ['abc', '123']})
+ * // true
+ * // return expected('123 abc').output({ has: ['bca', '321']})
+ * // false
+ * {@link helper.test.js Tests for member output}
+ *
+ * @param {String} str - String literal to evaluate
+ * @returns {Boolean|Undefined}
+ */
 function expected (str) {
   var memo = new WeakMap()
 
@@ -21,21 +43,25 @@ function expected (str) {
     has: (k) => (valid.object(k)) ? memo.has(k) : void 0
   }
   var shake = ({ s_, ndl_ }) => {
-    var delim = ' '
+    var delim = ' ' // split str by this delimiter
     /* eslint-disable */
+    // redact () [] : and \s (lint not identifying regex)
     var sane = /([\s()[\[^\]]:*)/mg
     /* eslint-enable */
     if (!valid.string(s_) || !valid.array(ndl_)) return void 0
+    // sanitize str, split into array, iterate and evaluate presence of matched elements
     var tree = s_.replace(sane, ' ').split(delim).filter(o => ndl_.indexOf(o) !== -1)
     return tree.length !== 0
   }
 
   return {
+    // match any given list of elements against
+    // the supplied string. See header documenation of function for examples.
     output: function output$ ({ has }) {
       if (!valid.string(str)) return void 0
       var prev = { str: str, has: has }
       var tree = shake({ s_: str, ndl_: has })
-      if (!cursor.has(prev)) cursor.set(prev, tree)
+      if (!cursor.has(prev)) cursor.set(prev, tree) // return as selector
       return cursor.get(prev)
     }
   }
