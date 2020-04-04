97efe4150e0b7bedbc0d5ad191880edb2c1947a0
Matteo Collina <hello@matteocollina.com>
2020-04-04 13:56:34 +0200

    Correctly forwards chindings to prettyPrint when using option (#810)
    Fixes https://github.com/pinojs/pino-pretty/issues/104

--- test/pretty.test.js
+++ test/pretty.test.js
@@ -112,6 +112,25 @@ test('parses and outputs chindings', async ({ is, isNot }) => {
   is(strip(actual).match(/a: 1/g).length, 3)
 })
 
+test('parses and outputs chindings with serializer', async ({ is, isNot }) => {
+  var actual = ''
+  const child = execa(process.argv[0], [join(__dirname, 'fixtures', 'pretty', 'child-with-serializer.js')])
+
+  child.stdout.pipe(writer((s, enc, cb) => {
+    actual += s
+    cb()
+  }))
+  await once(child, 'close')
+  isNot(strip(actual).match(/\(123456 on abcdefghijklmnopqr\): h/), null)
+  isNot(strip(actual).match(/\(123456 on abcdefghijklmnopqr\): h2/), null)
+  isNot(strip(actual).match(/\(123456 on abcdefghijklmnopqr\): h3/), null)
+  isNot(strip(actual).match(/\(123456 on abcdefghijklmnopqr\): h4/), null)
+  isNot(strip(actual).match(/a: 2/), null)
+  isNot(strip(actual).match(/a: 16/), null)
+  isNot(strip(actual).match(/a: 42/), null)
+  is(strip(actual).match(/a: /g).length, 4)
+})
+
 test('applies serializers', async ({ is, isNot }) => {
   var actual = ''
   const child = execa(process.argv[0], [join(__dirname, 'fixtures', 'pretty', 'serializers.js')])
