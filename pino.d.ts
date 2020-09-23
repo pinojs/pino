6a94cc2ec4973b8ba10311487ab5060fb6693b4c
Igor Savin <iselwin@gmail.com>
2020-09-23 13:28:14 +0300

    Include SonicBoom types again until there is a new version available

--- pino.d.ts
+++ pino.d.ts
@@ -18,7 +18,6 @@
 
 import { EventEmitter } from 'events';
 import * as pinoStdSerializers from 'pino-std-serializers';
-import SonicBoom from 'sonic-boom'
 
 export default P
 
@@ -729,4 +728,44 @@ export namespace P {
         censor?: string | ((v: any) => any);
         remove?: boolean;
     }
+}
+
+declare class SonicBoom extends EventEmitter {
+    /**
+     * @param [fileDescriptor] File path or numerical file descriptor
+     * relative protocol is enabled. Default: process.stdout
+     * @returns a new sonic-boom instance
+     */
+    constructor(fileDescriptor: string | number, minLength?: number, sync?: boolean)
+
+    /**
+     * Writes the string to the file. It will return false to signal the producer to slow down.
+     */
+    write(string: string): void;
+
+    /**
+     * Writes the current buffer to the file if a write was not in progress.
+     * Do nothing if minLength is zero or if it is already writing.
+     */
+    flush(): void;
+
+    /**
+     * Reopen the file in place, useful for log rotation.
+     */
+    reopen(fileDescriptor?: string | number): void;
+
+    /**
+     * Flushes the buffered data synchronously. This is a costly operation.
+     */
+    flushSync(): void;
+
+    /**
+     * Closes the stream, the data will be flushed down asynchronously
+     */
+    end(): void;
+
+    /**
+     * Closes the stream immediately, the data is not flushed.
+     */
+    destroy(): void;
 }
