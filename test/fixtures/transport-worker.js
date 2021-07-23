2d662c77779c6f56ab11c8db04074d2761e73c36
Matteo Collina <hello@matteocollina.com>
2021-07-23 15:00:06 +0200

    v12 support

--- test/fixtures/transport-worker.js
+++ test/fixtures/transport-worker.js
@@ -3,6 +3,7 @@
 const { Writable } = require('stream')
 module.exports = (options) => {
   const myTransportStream = new Writable({
+    autoDestroy: true,
     write (chunk, enc, cb) {
       console.log(chunk.toString())
       cb()
