67ceb5f509502b1c0685eb8803c8cda312cb8101
Matteo Collina <hello@matteocollina.com>
2021-08-20 15:50:52 +0200

    Automatically flush the logs on exit in Node v14+ (#1091)

--- test/final.test.js
+++ test/final.test.js
@@ -4,6 +4,10 @@ const fs = require('fs')
 const { test } = require('tap')
 const { sleep, getPathToNull } = require('./helper')
 
+// This test is too fast for the GC to trigger the removal, so a leak warning
+// will be emitted. Let's raise this so we do not scare everybody.
+process.setMaxListeners(100)
+
 test('replaces onTerminated option', async ({ throws }) => {
   throws(() => {
     pino({
@@ -48,6 +52,7 @@ test('listener function immediately sync flushes when fired (sync false)', async
   dest.flushSync = () => {
     passed = true
     pass('flushSync called')
+    dest.flushSync = () => {}
   }
   pino.final(pino(dest), () => {})()
   await sleep(10)
@@ -60,6 +65,7 @@ test('listener function immediately sync flushes when fired (sync true)', async 
   dest.flushSync = () => {
     passed = true
     pass('flushSync called')
+    dest.flushSync = () => {}
   }
   pino.final(pino(dest), () => {})()
   await sleep(10)
