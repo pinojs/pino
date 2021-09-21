d6e171caee4c178387678c77fcd9d020e8ed0254
KaKa <kaka@kakawebsitedemo.com>
2021-09-21 17:59:16 +0800

    fix: console transport

--- test/fixtures/console-transport.js
+++ test/fixtures/console-transport.js
@@ -1,12 +1,10 @@
 const { Writable } = require('stream')
-const { pass } = require('tap')
 
 module.exports = (options) => {
   const myTransportStream = new Writable({
     write (chunk, enc, cb) {
       // apply a transform and send to stdout
       console.log(chunk.toString().toUpperCase())
-      pass()
       cb()
     }
   })
