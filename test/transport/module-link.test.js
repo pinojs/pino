e9eb267d386cc7357c839913df403ef065adff29
Matteo Collina <hello@matteocollina.com>
2021-10-07 14:48:27 +0200

    Correctly select the correct file when using the transport option (#1147)

--- test/transport/module-link.test.js
+++ test/transport/module-link.test.js
@@ -2,24 +2,31 @@
 
 const os = require('os')
 const { join } = require('path')
-const { readFile, symlink, unlink } = require('fs').promises
+const { readFile, symlink, unlink, mkdir, writeFile } = require('fs').promises
 const { test } = require('tap')
 const { isWin, isYarnPnp, watchFileCreated } = require('../helper')
+const { once } = require('events')
+const execa = require('execa')
 const pino = require('../../')
 
 const { pid } = process
 const hostname = os.hostname()
 
-async function installTransportModule () {
+async function installTransportModule (target) {
   if (isYarnPnp) {
     return
   }
   try {
     await uninstallTransportModule()
   } catch {}
+
+  if (!target) {
+    target = join(__dirname, '..', '..')
+  }
+
   await symlink(
     join(__dirname, '..', 'fixtures', 'transport'),
-    join(__dirname, '..', '..', 'node_modules', 'transport')
+    join(target, 'node_modules', 'transport')
   )
 }
 
@@ -86,6 +93,54 @@ test('pino.transport with package as a target', { skip: isWin }, async ({ same, 
   delete result.time
   same(result, {
     pid,
+    hostname,
+    level: 30,
+    msg: 'hello'
+  })
+})
+
+// TODO make this test pass on Windows
+test('pino({ transport })', { skip: isWin || isYarnPnp }, async ({ same, teardown }) => {
+  const folder = join(
+    os.tmpdir(),
+    '_' + Math.random().toString(36).substr(2, 9)
+  )
+
+  const destination = join(folder, 'output')
+
+  await mkdir(join(folder, 'node_modules'), { recursive: true })
+
+  // Link pino
+  await symlink(
+    join(__dirname, '..', '..'),
+    join(folder, 'node_modules', 'pino')
+  )
+
+  await installTransportModule(folder)
+
+  const toRun = join(folder, 'index.js')
+
+  const toRunContent = `
+    const pino = require('pino')
+    const logger = pino({
+      transport: {
+        target: 'transport',
+        options: { destination: '${destination}' }
+      }
+    })
+    logger.info('hello')
+  `
+
+  await writeFile(toRun, toRunContent)
+
+  const child = execa(process.argv[0], [toRun])
+
+  await once(child, 'close')
+
+  const result = JSON.parse(await readFile(destination))
+  delete result.time
+  same(result, {
+    pid: child.pid,
     hostname,
     level: 30,
     msg: 'hello'
