503961642ec42b6431651e0913c919194b29dc77
Matteo Collina <hello@matteocollina.com>
2021-11-01 08:08:53 +0100

    Update examples/basic.js
    Co-authored-by: James Sumners <james@sumners.email>

--- examples/basic.js
+++ examples/basic.js
@@ -1,7 +1,13 @@
 'use strict'
 
-// const pino = require('..')(require('pino-pretty')())
+// Pino's primary usage writes ndjson to `stdout`:
 const pino = require('..')()
+
+// However, if "human readable" output is desired,
+// `pino-pretty` can be provided as the destination
+// stream by uncommenting the following line in place
+// of the previous declaration:
+// const pino = require('..')(require('pino-pretty')())
 
 pino.info('hello world')
 pino.error('this is at error level')
