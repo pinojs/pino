6c538159dc83b190889781f80051962138714bb8
Sameer Srivastava <sameer.srivastava@nearform.com>
2021-11-02 21:39:11 +0530

    Deprecation warning for pino.final() in Node v14+ (#1199)

--- test/final.test.js
+++ test/final.test.js
@@ -8,6 +8,29 @@ const { sleep, getPathToNull } = require('./helper')
 // will be emitted. Let's raise this so we do not scare everybody.
 process.setMaxListeners(100)
 
+test('should show warning for pino.final on node 14+', ({ equal, end, plan }) => {
+  const major = Number(process.versions.node.split('.')[0])
+  if (major < 14) return end()
+
+  plan(1)
+  const dest = pino.destination({ dest: getPathToNull(), sync: false })
+  dest.flushSync = () => {}
+  const instance = pino(dest)
+
+  pino.final(instance, (_, finalLogger) => {
+    finalLogger.info('hello')
+  })()
+
+  function onWarning (warning) {
+    equal(warning.code, 'PINODEP009')
+    end()
+  }
+
+  process.once('warning', onWarning)
+
+  instance.info('hello')
+})
+
 test('replaces onTerminated option', async ({ throws }) => {
   throws(() => {
     pino({
