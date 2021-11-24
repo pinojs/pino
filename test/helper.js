87558fd318d508b001c8133a2572310abad3e94a
Michael Dawson <mdawson@devrus.com>
2021-11-24 09:26:46 -0500

    Update test/helper.js
    Co-authored-by: Matteo Collina <matteo.collina@gmail.com>

--- test/helper.js
+++ test/helper.js
@@ -79,7 +79,7 @@ function watchFileCreated (filename) {
 
 function watchForWrite (filename, testString) {
   return new Promise((resolve, reject) => {
-    const TIMEOUT = process.env.PINO_TEST_WAIT_WRITE_TIMEOUT || 2000
+    const TIMEOUT = process.env.PINO_TEST_WAIT_WRITE_TIMEOUT || 10000
     const INTERVAL = 100
     const threshold = TIMEOUT / INTERVAL
     let counter = 0
