8dceecdeb6eb4e96b348277bf908496b7880917a
Matteo Collina <hello@matteocollina.com>
2022-01-10 15:32:06 +0100

    Call .end() on thread-stream before exiting the process. (#1296)

--- test/fixtures/transport-exit-immediately-with-async-dest.js
+++ test/fixtures/transport-exit-immediately-with-async-dest.js
@@ -2,10 +2,15 @@
 
 const pino = require('../..')
 const transport = pino.transport({
-  target: 'pino/file'
+  target: './to-file-transport-with-transform.js',
+  options: {
+    destination: process.argv[2]
+  }
 })
 const logger = pino(transport)
 
 logger.info('Hello')
+
+logger.info('World')
 
 process.exit(0)
