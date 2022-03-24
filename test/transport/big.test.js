27480f2feb4956c70692e3af8f394dfe4da2db13
Andres Correa Casablanca <andres.casablanca@nearform.com>
2022-03-24 10:36:40 +0100

    test: use sync instead of sleep

--- test/transport/big.test.js
+++ test/transport/big.test.js
@@ -17,9 +17,19 @@ const sleep = promisify(setTimeout)
 test('eight million lines', async ({ equal, comment }) => {
   const destination = file()
   const child = execa(process.argv[0], [join(__dirname, '..', 'fixtures', 'transport-many-lines.js'), destination])
-
   await once(child, 'exit')
-  await sleep(1000) // wait for the file to be written
+
+  if (process.platform === 'win32') {
+    await sleep(1000) // In Windows we don't have the POSIX `sync` command
+  } else {
+    try {
+      const sync = execa('sync') // Wait for the file to be writen to disk
+      await once(sync, 'exit')
+    } catch {
+      await sleep(1000) // Just a fallback, but this should be unreachable
+    }
+  }
+
   const toWrite = 8 * 1000000
   let count = 0
   await pipeline(createReadStream(destination), split(), new Writable({
