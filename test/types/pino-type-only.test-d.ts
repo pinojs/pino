53c914c6a68982eb5ff9504406f4f39fb68819e1
Deon Groenewald <deon.groenewald@lever.co>
2022-04-08 17:35:08 -0400

    Export `LevelWithSilent` (#1399)

--- test/types/pino-type-only.test-d.ts
+++ test/types/pino-type-only.test-d.ts
@@ -1,7 +1,7 @@
-import { expectType } from "tsd";
+import { expectAssignable, expectType } from "tsd";
 
 import pino from "../../";
-import type {Logger, LogFn, P} from "../../pino";
+import type {LevelWithSilent, Logger, LogFn, P} from "../../pino";
 
 // NB: can also use `import * as pino`, but that form is callable as `pino()`
 // under `esModuleInterop: false` or `pino.default()` under `esModuleInterop: true`.
@@ -11,3 +11,6 @@ expectType<LogFn>(log.info);
 
 expectType<P.Logger>(log);
 expectType<P.LogFn>(log.info);
+
+const level: LevelWithSilent = 'silent';
+expectAssignable<P.LevelWithSilent>(level);
