bcdd665c30adff6793428794bb314eeddfada9c0
Vadim Budarin <budarin-vv@yandex.ru>
2022-06-10 06:05:42 +0300

    fix: pino instead fastify

--- test/internals/version.test.js
+++ test/internals/version.test.js
@@ -4,12 +4,12 @@ const fs = require('fs')
 const path = require('path')
 const t = require('tap')
 const test = t.test
-const fastify = require('../..')()
+const pino = require('../..')()
 
 test('should be the same as package.json', t => {
   t.plan(1)
 
   const json = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'package.json')).toString('utf8'))
 
-  t.equal(fastify.version, json.version)
+  t.equal(pino.version, json.version)
 })
