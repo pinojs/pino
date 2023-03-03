856f09901916c8eefb68cfe0bf4d8955be2fddb1
couragecowardlydog <vivekanandansakthivelu@gmail.com>
2023-03-03 21:17:25 +0530

    Update smoke.js

--- smoke.js
+++ smoke.js
@@ -1,13 +1,5 @@
 'use strict'
-
-// Pino's primary usage writes ndjson to `stdout`:
 const pino = require('pino')()
-
-// However, if "human readable" output is desired,
-// `pino-pretty` can be provided as the destination
-// stream by uncommenting the following line in place
-// of the previous declaration:
-// const pino = require('..')(require('pino-pretty')())
 
 pino.info('hello world')
 pino.error('this is at error level')
