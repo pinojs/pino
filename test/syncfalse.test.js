907d8209d939e59f05f6e49b7738dcfae3d4fa89
Matteo Collina <hello@matteocollina.com>
2023-10-09 09:04:40 +0200

    Fix Node.js in CITGM (v18.x)
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/syncfalse.test.js
+++ test/syncfalse.test.js
@@ -7,6 +7,7 @@ const { test } = require('tap')
 const { fork } = require('child_process')
 const writer = require('flush-write-stream')
 const { once, getPathToNull } = require('./helper')
+const { setImmediate } = require('timers/promises')
 
 test('asynchronous logging', async ({ equal, teardown }) => {
   const now = Date.now
@@ -48,6 +49,8 @@ test('asynchronous logging', async ({ equal, teardown }) => {
     cb()
   }))
   await once(child, 'close')
+  // Wait for the last write to be flushed
+  await setImmediate()
   equal(actual, expected)
   equal(actual2.trim(), expected2)
 
