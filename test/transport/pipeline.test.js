c3e9fec553d0c869f7a72cc43d1524e448586d6a
Daniele Bacarella <daniele.bacarella@gmail.com>
2024-05-05 11:52:50 +0200

    - Reverted changes related to the support of named pipelines - Implemented support for mixed target&pipeline definitions  within `targets` in `transport.js` - Merged logic from both `worker.js` and `worker-pipeline.js` into `worker.js` - Fixed `pipeline.test.js` - Fixed docs to reflect changes above
    TODO:
     - Remove `worker-pipeline.js`
     - Fix `transport.js` to use only `worker.js`
     - Fix related docs
     - Fix UTs

--- test/transport/pipeline.test.js
+++ test/transport/pipeline.test.js
@@ -10,14 +10,6 @@ const pino = require('../../')
 const { pid } = process
 const hostname = os.hostname()
 
-/**
- * The idea to address the requirement of mix&match target and pipeline
- * https://github.com/pinojs/pino/issues/1302
- * is to implement a sort of `tee`.
- * Targets defined in `pipelines` property will be used to generate
- * an output value which will be then written to different end targets.
- *
- */
 test('pino.transport with a pipeline', async ({ same, teardown }) => {
   const destination = file()
   const transport = pino.transport({
@@ -43,7 +35,7 @@ test('pino.transport with a pipeline', async ({ same, teardown }) => {
   })
 })
 
-test('pino.transport with targets using a shared pipeline', async ({ same, teardown }) => {
+test('pino.transport with targets containing pipelines', async ({ same, teardown }) => {
   const destinationA = join(
     os.tmpdir(),
     '_' + Math.random().toString(36).substr(2, 9)
@@ -56,22 +48,20 @@ test('pino.transport with targets using a shared pipeline', async ({ same, teard
     targets: [
       {
         target: join(__dirname, '..', 'fixtures', 'to-file-transport.js'),
-        options: { destination: destinationA },
-        pipeline: 'pipelineA'
+        options: { destination: destinationA }
       },
       {
-        target: join(__dirname, '..', 'fixtures', 'to-file-transport.js'),
-        options: { destination: destinationB },
-        pipeline: 'pipelineA'
+        pipeline: [
+          {
+            target: join(__dirname, '..', 'fixtures', 'transport-transform.js')
+          },
+          {
+            target: join(__dirname, '..', 'fixtures', 'to-file-transport.js'),
+            options: { destination: destinationB }
+          }
+        ]
       }
-    ],
-    pipelines: {
-      pipelineA: [
-        {
-          target: join(__dirname, '..', 'fixtures', 'transport-transform.js')
-        }
-      ]
-    }
+    ]
   })
 
   teardown(transport.end.bind(transport))
@@ -84,128 +74,16 @@ test('pino.transport with targets using a shared pipeline', async ({ same, teard
   delete resultA.time
   delete resultB.time
   same(resultA, {
-    pid,
-    hostname,
-    level: 30,
-    msg: 'hello',
-    service: 'pino' // this property was added by the transform
-  })
-  same(resultB, {
-    pid,
-    hostname,
-    level: 30,
-    msg: 'hello',
-    service: 'pino' // this property was added by the transform
-  })
-})
-
-test('pino.transport with shared pipeline and target with pipeline=undefined', async ({ same, teardown }) => {
-  const destinationA = join(
-    os.tmpdir(),
-    '_' + Math.random().toString(36).substr(2, 9)
-  )
-  const destinationB = join(
-    os.tmpdir(),
-    '_' + Math.random().toString(36).substr(2, 9)
-  )
-  const transport = pino.transport({
-    targets: [
-      {
-        target: join(__dirname, '..', 'fixtures', 'to-file-transport.js'),
-        options: { destination: destinationA },
-        pipeline: 'pipelineA'
-      },
-      {
-        target: join(__dirname, '..', 'fixtures', 'to-file-transport.js'),
-        options: { destination: destinationB }
-      }
-    ],
-    pipelines: {
-      pipelineA: [
-        {
-          target: join(__dirname, '..', 'fixtures', 'transport-transform.js')
-        }
-      ]
-    }
-  })
-
-  teardown(transport.end.bind(transport))
-  const instance = pino(transport)
-  instance.info('hello')
-  await watchFileCreated(destinationA)
-  await watchFileCreated(destinationB)
-  const resultA = JSON.parse(await readFile(destinationA))
-  const resultB = JSON.parse(await readFile(destinationB))
-  delete resultA.time
-  delete resultB.time
-  same(resultA, {
-    pid,
-    hostname,
-    level: 30,
-    msg: 'hello',
-    service: 'pino' // this property was added by the transform
-  })
-  same(resultB, {
     pid,
     hostname,
     level: 30,
     msg: 'hello'
   })
+  same(resultB, {
+    pid,
+    hostname,
+    level: 30,
+    msg: 'hello',
+    service: 'pino' // this property was added by the transform
+  })
 })
-
-// test('pino.transport with target using a custom pipeline', async ({ same, teardown }) => {
-//   const destinationA = join(
-//     os.tmpdir(),
-//     '_' + Math.random().toString(36).substr(2, 9)
-//   )
-//   const destinationB = join(
-//     os.tmpdir(),
-//     '_' + Math.random().toString(36).substr(2, 9)
-//   )
-//   const transport = pino.transport({
-//     targets: [
-//       {
-//         target: join(__dirname, '..', 'fixtures', 'to-file-transport.js'),
-//         options: { destination: destinationA },
-//         pipeline: [{
-//           target: join(__dirname, '..', 'fixtures', 'transport-transform.js'),
-//           options: { payload: 'customPipeline' }
-//         }]
-//       },
-//       {
-//         target: join(__dirname, '..', 'fixtures', 'to-file-transport.js'),
-//         options: { destination: destinationB }
-//       }
-//     ],
-//     pipeline: [
-//       {
-//         target: join(__dirname, '..', 'fixtures', 'transport-transform.js'),
-//         options: { payload: 'globalPipeline' }
-//       }
-//     ]
-//   })
-
-//   teardown(transport.end.bind(transport))
-//   const instance = pino(transport)
-//   instance.info('hello')
-//   await watchFileCreated(destinationA)
-//   await watchFileCreated(destinationB)
-//   const resultA = JSON.parse(await readFile(destinationA))
-//   const resultB = JSON.parse(await readFile(destinationB))
-//   delete resultA.time
-//   delete resultB.time
-//   same(resultA, {
-//     pid,
-//     hostname,
-//     level: 30,
-//     msg: 'hello',
-//     service: 'customPipeline' // this property was added by the transform
-//   })
-//   same(resultB, {
-//     pid,
-//     hostname,
-//     level: 30,
-//     msg: 'hello',
-//     service: 'globalPipeline' // this property was added by the transform
-//   })
-// })
