c0a320b5af9adb6e9c51cd9527d9dcb849e31774
Ondřej Langr <hi@ondrejlangr.cz>
2024-10-07 12:46:59 +0200

    fix: Fixes lowercasing the custom levels when useOnlyCustomLevels is true.

--- pino.js
+++ pino.js
@@ -89,7 +89,13 @@ function pino (...args) {
   const instance = {}
   const { opts, stream } = normalize(instance, caller(), ...args)
 
-  if (opts.level && typeof opts.level === 'string' && DEFAULT_LEVELS[opts.level.toLowerCase()] !== undefined) opts.level = opts.level.toLowerCase()
+  if (
+    opts.level && 
+    typeof opts.level === 'string' && 
+    DEFAULT_LEVELS[opts.level.toLowerCase()] !== undefined && 
+    !opts.useOnlyCustomLevels
+  ) 
+    opts.level = opts.level.toLowerCase()
 
   const {
     redact,
