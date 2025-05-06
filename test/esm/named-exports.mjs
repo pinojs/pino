75d8ae9401366d4a128a3c53a3286d7f8780b909
Luca Del Puppo <Puppo@users.noreply.github.com>
2025-05-06 12:23:37 +0200

    test: move esm to node test (#2199)

--- test/esm/named-exports.mjs
+++ test/esm/named-exports.mjs
@@ -1,26 +1,26 @@
-import { hostname } from 'node:os'
-import t from 'tap'
-import { sink, check, once, watchFileCreated, file } from '../helper.js'
-import { pino, destination } from '../../pino.js'
 import { readFileSync } from 'node:fs'
+import { hostname } from 'node:os'
+import t from 'node:test'
+import { destination, pino } from '../../pino.js'
+import { check, file, once, sink, watchFileCreated } from '../helper.js'
 
-t.test('named exports support', async ({ equal }) => {
+t.test('named exports support', async (t) => {
   const stream = sink()
   const instance = pino(stream)
   instance.info('hello world')
-  check(equal, await once(stream, 'data'), 30, 'hello world')
+  check(t.assert.strictEqual, await once(stream, 'data'), 30, 'hello world')
 })
 
-t.test('destination', async ({ same }) => {
+t.test('destination', async (t) => {
   const tmp = file()
   const instance = pino(destination(tmp))
   instance.info('hello')
   await watchFileCreated(tmp)
   const result = JSON.parse(readFileSync(tmp).toString())
   delete result.time
-  same(result, {
+  t.assert.deepStrictEqual(result, {
     pid: process.pid,
-    hostname,
+    hostname: hostname(),
     level: 30,
     msg: 'hello'
   })
