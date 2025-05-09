840e145c199d8e3d7d020e15576e7e20b4ee88c7
Luca Del Puppo <Puppo@users.noreply.github.com>
2025-05-09 13:21:42 +0200

    test: move transport-stream.test.js to node test (#2216)
    * test: move transport-stream.test.js to node test
    
    * use dependency injection
    
    ---------
    
    Co-authored-by: James Sumners <git@zifbang.com>

--- test/transport-stream.test.js
+++ test/transport-stream.test.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { test } = require('tap')
+const { test } = require('node:test')
 
 test('should import', async (t) => {
   t.plan(2)
@@ -8,19 +8,16 @@ test('should import', async (t) => {
     return {
       default: {
         default: () => {
-          t.equal(target, 'pino-pretty')
+          t.assert.strictEqual(target, 'pino-pretty')
           return Promise.resolve()
         }
       }
     }
   }
   const mockRealImport = async () => { await Promise.resolve(); throw Object.assign(new Error(), { code: 'ERR_MODULE_NOT_FOUND' }) }
+  const loadTransportStreamBuilder = require('../lib/transport-stream.js')
 
-  /** @type {typeof import('../lib/transport-stream.js')} */
-  const loadTransportStreamBuilder = t.mock('../lib/transport-stream.js', { 'real-require': { realRequire: mockRealRequire, realImport: mockRealImport } })
+  const fn = await loadTransportStreamBuilder('pino-pretty', { realImport: mockRealImport, realRequire: mockRealRequire })
 
-  const fn = await loadTransportStreamBuilder('pino-pretty')
-
-  t.resolves(fn())
-  t.end()
+  t.assert.doesNotThrow(() => fn())
 })
