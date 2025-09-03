7e074b3c75165f62e70137136c3ac82082ba4ab6
Matteo Collina <hello@matteocollina.com>
2025-09-03 09:43:06 +0200

    automate releases
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- build/sync-version.js
+++ build/sync-version.js
@@ -1,6 +1,21 @@
+'use strict'
+
 const fs = require('node:fs')
 const path = require('node:path')
-const { version } = require('../package.json')
+let { version } = require('../package.json')
+
+let passedVersion = process.argv[2]
+
+if (passedVersion) {
+  passedVersion = passedVersion.trim().replace(/^v/, '')
+  if (version != passedVersion) {
+    console.log(`Syncing version from ${version} to ${passedVersion}`)
+    version = passedVersion
+    const packageJson = require('../package.json')
+    packageJson.version = version
+    fs.writeFileSync(path.resolve('./package.json'), JSON.stringify(packageJson, null, 2) + '\n', { encoding: 'utf-8' })
+  }
+}
 
 const metaContent = `'use strict'
 
