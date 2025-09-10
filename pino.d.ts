e2bfc9667849f2d0eb9471272fdd047c361231b1
Matteo Collina <hello@matteocollina.com>
2025-09-10 15:02:03 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- pino.d.ts
+++ pino.d.ts
@@ -326,7 +326,7 @@ declare namespace pino {
     type PlaceholderTypeMapping<T extends PlaceholderSpecifier> = T extends 'd'
         ? number
         : T extends 's'
-            ? string | number | boolean | bigint | null | undefined | symbol | object
+            ? unknown
             : T extends 'j' | 'o' | 'O'
             ? object
             : never;
