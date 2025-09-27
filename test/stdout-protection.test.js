84521fdfb0352049c290f34699748299f29dadd4
James Sumners <git@zifbang.com>
2025-09-27 13:05:50 -0400

    stdout-protection.test.js

--- test/stdout-protection.test.js
+++ test/stdout-protection.test.js
@@ -1,13 +1,15 @@
 'use strict'
 
-const { test } = require('tap')
+const test = require('node:test')
+const assert = require('node:assert')
 const { join } = require('node:path')
 const { fork } = require('node:child_process')
-const { once } = require('./helper')
 const writer = require('flush-write-stream')
+
+const { once } = require('./helper')
 const pino = require('..')
 
-test('do not use SonicBoom is someone tampered with process.stdout.write', async ({ not }) => {
+test('do not use SonicBoom is someone tampered with process.stdout.write', async () => {
   let actual = ''
   const child = fork(join(__dirname, 'fixtures', 'stdout-hack-protection.js'), { silent: true })
 
@@ -16,24 +18,24 @@ test('do not use SonicBoom is someone tampered with process.stdout.write', async
     cb()
   }))
   await once(child, 'close')
-  not(actual.match(/^hack/), null)
+  assert.equal(actual.match(/^hack/) != null, true)
 })
 
-test('do not use SonicBoom is someone has passed process.stdout to pino', async ({ equal }) => {
+test('do not use SonicBoom is someone has passed process.stdout to pino', async () => {
   const logger = pino(process.stdout)
-  equal(logger[pino.symbols.streamSym], process.stdout)
+  assert.equal(logger[pino.symbols.streamSym], process.stdout)
 })
 
-test('do not crash if process.stdout has no fd', async ({ teardown }) => {
+test('do not crash if process.stdout has no fd', async (t) => {
   const fd = process.stdout.fd
   delete process.stdout.fd
-  teardown(function () { process.stdout.fd = fd })
+  t.after(function () { process.stdout.fd = fd })
   pino()
 })
 
-test('use fd=1 if process.stdout has no fd in pino.destination() (worker case)', async ({ teardown }) => {
+test('use fd=1 if process.stdout has no fd in pino.destination() (worker case)', async (t) => {
   const fd = process.stdout.fd
   delete process.stdout.fd
-  teardown(function () { process.stdout.fd = fd })
+  t.after(function () { process.stdout.fd = fd })
   pino.destination()
 })
