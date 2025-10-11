90fd3d7d2256845154998f17bc5e3d8df87be190
CarlOlson <CarlOlson@users.noreply.github.com>
2025-10-11 13:55:56 +0900

    convert to node:test

--- test/caller.test.js
+++ test/caller.test.js
@@ -1,21 +1,21 @@
-const { test } = require('tap')
+'use strict'
+
+const test = require('node:test')
+const assert = require('node:assert')
 const loop = require('./fixtures/caller-loop.js')
 
-test('caller', async ({ test, end }) => {
-  test('returns a callstack of absolute paths', async ({ equal }) => {
-    const callers = loop(7).map(fileName => fileName.substring(__dirname.length))
+test('returns a callstack of absolute paths', () => {
+  const callers = loop(7).map(fileName => fileName.substring(__dirname.length))
 
-    // default callstack size is 10, but the top 2 are dropped
-    equal(callers.length, 8)
-    equal(callers[0], '/fixtures/caller-loop.js')
-    equal(callers[1], '/fixtures/caller-loop.js')
-    equal(callers[2], '/fixtures/caller-loop.js')
-    equal(callers[3], '/fixtures/caller-loop.js')
-    equal(callers[4], '/fixtures/caller-loop.js')
-    equal(callers[5], '/fixtures/caller-loop.js')
-    equal(callers[6], '/fixtures/caller-loop.js')
-    equal(callers[7], '/caller.test.js')
-  })
-
-  end()
+  // default callstack size is 10, but the top 2 are dropped
+  assert.deepStrictEqual(callers, [
+    '/fixtures/caller-loop.js',
+    '/fixtures/caller-loop.js',
+    '/fixtures/caller-loop.js',
+    '/fixtures/caller-loop.js',
+    '/fixtures/caller-loop.js',
+    '/fixtures/caller-loop.js',
+    '/fixtures/caller-loop.js',
+    '/caller.test.js',
+  ])
 })
