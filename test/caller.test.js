c6d6ad6af65d9f0b6dc7bd31bc2d5a8e8814a07e
CarlOlson <CarlOlson@users.noreply.github.com>
2025-10-11 22:10:09 +0900

    fix test on windows

--- test/caller.test.js
+++ test/caller.test.js
@@ -8,14 +8,13 @@ test('returns a callstack of absolute paths', () => {
   const callers = loop(7).map(fileName => fileName.substring(__dirname.length))
 
   // default callstack size is 10, but the top 2 are dropped
-  assert.deepStrictEqual(callers, [
-    '/fixtures/caller-loop.js',
-    '/fixtures/caller-loop.js',
-    '/fixtures/caller-loop.js',
-    '/fixtures/caller-loop.js',
-    '/fixtures/caller-loop.js',
-    '/fixtures/caller-loop.js',
-    '/fixtures/caller-loop.js',
-    '/caller.test.js',
-  ])
+  assert.equal(callers.length, 8)
+  assert.match(callers[0], /^[\/\\]fixtures[\/\\]caller-loop\.js$/)
+  assert.match(callers[1], /^[\/\\]fixtures[\/\\]caller-loop\.js$/)
+  assert.match(callers[2], /^[\/\\]fixtures[\/\\]caller-loop\.js$/)
+  assert.match(callers[3], /^[\/\\]fixtures[\/\\]caller-loop\.js$/)
+  assert.match(callers[4], /^[\/\\]fixtures[\/\\]caller-loop\.js$/)
+  assert.match(callers[5], /^[\/\\]fixtures[\/\\]caller-loop\.js$/)
+  assert.match(callers[6], /^[\/\\]fixtures[\/\\]caller-loop\.js$/)
+  assert.match(callers[7], /^[\/\\]caller\.test\.js$/)
 })
